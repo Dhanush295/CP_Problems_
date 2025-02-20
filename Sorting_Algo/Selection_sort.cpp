9666d118a5bcfb3a41259a7933b0ffefa29490bd
Dhanush <81304542+Dhanush295@users.noreply.github.com>
2025-02-19 20:34:49 -0800

    Create Selection_sort.cpp

---
+++ Sorting_Algo/Selection_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+void selection_sort(int arr[], int n){
+  for(int i = 0; i<n-1; i++){
+    int minimum = i;
+    for(int j=i; j<n; j++){
+      if(arr[j] < arr[minimum]) minimum = j;
+    }
+  }
+  swap(arr[i], arr[minimum]);
+}
+
+int main(){
+  int arr[6] = {13, 46, 24, 52, 20, 9};
+    int n = 6;
+    selection_sort(arr, 6);
+
+    for(int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+
+  return 0;
+}
